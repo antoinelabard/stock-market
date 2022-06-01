c7c2b00bb08230ac5ac246a544a476b5e87095ac
Antoine Labard <antoine@labard.fr>
2022-06-01 15:30:43 +0200

    feat: ✨ add StockApi.kt interface

---
+++ app/src/main/java/fr/labard/stockmarket/data/remote/StockApi.kt
@@ -0,0 +1,18 @@
+package fr.labard.stockmarket.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("quer?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_key = "BFYNRU4N5LY3N4ZN"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
