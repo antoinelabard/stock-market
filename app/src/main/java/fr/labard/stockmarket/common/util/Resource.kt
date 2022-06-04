b6d82b9486658b430486aded0a9ca879e42e84a1
Antoine Labard <antoine@labard.fr>
2022-06-04 17:55:28 +0200

    feat: 👔 implement Resource.kt

---
+++ app/src/main/java/fr/labard/stockmarket/common/util/Resource.kt
@@ -0,0 +1,7 @@
+package fr.labard.stockmarket.common.util
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?) : Resource<T>(data = data)
+    class Error<T>(data: T?, message: String?) : Resource<T>(data = data, message = message)
+    class Loading<T>(val isLoading: Boolean = true) : Resource<T>()
+}
