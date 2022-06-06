fac2e5da60cbf557cf0ed61c17d06c2e2ea00b12
Antoine Labard <antoine@labard.fr>
2022-06-06 17:09:02 +0200

    feat: ✨ add CompanyInfo mapper

--- app/src/main/java/fr/labard/stockmarket/data/mapper/CompanyMapper.kt
+++ app/src/main/java/fr/labard/stockmarket/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package fr.labard.stockmarket.data.mapper
 
 import fr.labard.stockmarket.data.local.CompanyListingEntity
+import fr.labard.stockmarket.data.remote.dto.CompanyInforDto
+import fr.labard.stockmarket.domain.model.CompanyInfo
 import fr.labard.stockmarket.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing() = CompanyListing(
@@ -14,3 +16,13 @@ fun CompanyListing.toCompanyListingEntity() = CompanyListingEntity(
     symbol = symbol,
     exchange = exchange
 )
+
+fun CompanyInforDto.toCompanyInfo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: ""
+    )
+}
