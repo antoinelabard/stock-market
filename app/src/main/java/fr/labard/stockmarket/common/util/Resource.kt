421e2bf6e6ca5bb90656135c6f56711793eb9924
Antoine Labard <antoine@labard.fr>
2022-06-06 08:29:30 +0200

    fix: 🐛 add null value by default to data in Resource.Error

--- app/src/main/java/fr/labard/stockmarket/common/util/Resource.kt
+++ app/src/main/java/fr/labard/stockmarket/common/util/Resource.kt
@@ -2,6 +2,6 @@ package fr.labard.stockmarket.common.util
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T?) : Resource<T>(data = data)
-    class Error<T>(data: T?, message: String?) : Resource<T>(data = data, message = message)
+    class Error<T>(data: T? = null, message: String?) : Resource<T>(data = data, message = message)
     class Loading<T>(val isLoading: Boolean = true) : Resource<T>()
 }
