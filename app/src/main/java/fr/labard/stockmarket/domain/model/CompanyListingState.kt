be7dfe88dcda6f850f95eb56436098a09dd44aa5
Antoine Labard <antoine@labard.fr>
2022-06-06 10:10:42 +0200

    feat: ✨ implement CompanyListingsEvent.kt and CompanyListingState.kt

---
+++ app/src/main/java/fr/labard/stockmarket/domain/model/CompanyListingState.kt
@@ -0,0 +1,9 @@
+package fr.labard.stockmarket.domain.model
+
+data class CompanyListingState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+) {
+}
---
+++ app/src/main/java/fr/labard/stockmarket/presentation/company_listings/CompanyListingsEvent.kt
@@ -0,0 +1,6 @@
+package fr.labard.stockmarket.presentation.company_listings
+
+sealed class CompanyListingsEvent {
+    object Refresh : CompanyListingsEvent()
+    data class OnSearchQueryChange(val query: String) : CompanyListingsEvent()
+}
