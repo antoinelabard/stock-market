c9e79f15d960f7bb573f5c1a0f9545a662fcf895
Antoine Labard <antoine@labard.fr>
2022-06-06 16:51:32 +0200

    fix: 🩹 make CompanyInforDto fields nullable

--- app/src/main/java/fr/labard/stockmarket/data/remote/dto/CompanyInforDto.kt
+++ app/src/main/java/fr/labard/stockmarket/data/remote/dto/CompanyInforDto.kt
@@ -3,9 +3,9 @@ package fr.labard.stockmarket.data.remote.dto
 import com.squareup.moshi.Json
 
 data class CompanyInforDto(
-    @field:Json(name = "Name") val name: String,
-    @field:Json(name = "Symbol") val symbol: String,
-    @field:Json(name = "Description") val description: String,
-    @field:Json(name = "Country") val country: String,
-    @field:Json(name = "Industry") val industry: String,
+    @field:Json(name = "Name") val name: String?,
+    @field:Json(name = "Symbol") val symbol: String?,
+    @field:Json(name = "Description") val description: String?,
+    @field:Json(name = "Country") val country: String?,
+    @field:Json(name = "Industry") val industry: String?,
 )
