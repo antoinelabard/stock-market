84f325092752c0e3d007984d9f0e0b0b55455a20
Antoine Labard <antoine@labard.fr>
2022-06-06 14:46:14 +0200

    fix: 🐛 correct misplaced annotations

--- app/src/main/java/fr/labard/stockmarket/data/local/StockDao.kt
+++ app/src/main/java/fr/labard/stockmarket/data/local/StockDao.kt
@@ -1,9 +1,11 @@
 package fr.labard.stockmarket.data.local
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface StockDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
--- app/src/main/java/fr/labard/stockmarket/di/AppModule.kt
+++ app/src/main/java/fr/labard/stockmarket/di/AppModule.kt
@@ -2,6 +2,7 @@ package fr.labard.stockmarket.di
 
 import android.app.Application
 import androidx.room.Room
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -12,7 +13,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
-@Singleton
+@Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
