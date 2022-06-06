86670655b2e67520423a47a58175075b72bc9faa
Antoine Labard <antoine@labard.fr>
2022-06-06 16:28:45 +0200

    feat: 👔 add CompanyInfo and CompanyInforDto

---
+++ app/src/main/java/fr/labard/stockmarket/domain/model/CompanyInfo.kt
@@ -0,0 +1,9 @@
+package fr.labard.stockmarket.domain.model
+
+data class CompanyInfo(
+    val name: String,
+    val symbol: String,
+    val description: String,
+    val country: String,
+    val industry: String,
+)
