41776f369aa880226232fc31bf11dbebc72575ef
Antoine Labard <antoine@labard.fr>
2022-06-08 08:24:23 +0200

    feat: ✨ implement new api calls methods for StockApi

--- app/src/main/java/fr/labard/stockmarket/data/remote/StockApi.kt
+++ app/src/main/java/fr/labard/stockmarket/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package fr.labard.stockmarket.data.remote
 
+import fr.labard.stockmarket.data.remote.dto.CompanyInforDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,6 +11,18 @@ interface StockApi {
     suspend fun getListings(
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): CompanyInforDto
 
     companion object {
         const val API_KEY = "BFYNRU4N5LY3N4ZN"
