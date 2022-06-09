b1ff121668152899b59b5f7e8a4cd2f4ac154a62
Antoine Labard <antoine@labard.fr>
2022-06-09 07:32:55 +0200

    feat: ✨ add CompanyInfoState.kt

---
+++ app/src/main/java/fr/labard/stockmarket/presentation/company_info/CompanyInfoState.kt
@@ -0,0 +1,12 @@
+package fr.labard.stockmarket.presentation.company_info
+
+import fr.labard.stockmarket.domain.model.CompanyInfo
+import fr.labard.stockmarket.domain.model.IntradayInfo
+
+data class CompanyInfoState(
+    val stockInfos: List<IntradayInfo> = emptyList(),
+    val company: CompanyInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+) {
+}
