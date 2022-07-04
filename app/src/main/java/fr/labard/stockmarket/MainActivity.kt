5bb3ca1e4f551478fb611f7ae68d034d3b4390a5
Antoine Labard <antoine@labard.fr>
2022-07-04 16:39:12 +0200

    feat: 💄 add an top appbar to the app UI

--- app/src/main/java/fr/labard/stockmarket/MainActivity.kt
+++ app/src/main/java/fr/labard/stockmarket/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+                    Scaffold(
+                        topBar = { TopAppBar({ Text("Stock Market App") }) },
+                        content = { DestinationsNavHost(navGraph = NavGraphs.root) }
+                    )
                 }
             }
         }
